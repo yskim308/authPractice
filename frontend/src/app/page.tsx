4bec04ffdefe82508d39ed062130efe974ce5d87
Young Seo Kim <youngseokim7@gmail.com>
2025-04-15 21:32:34 +0900

    add signin button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+import SignIn from "./SignIn";
+
 export default function Home() {
   return (
     <>
       <div>
         <h1>hello world</h1>
         <p>ok so down below i will have the sign in button?</p>
+        <SignIn />
       </div>
     </>
   );
